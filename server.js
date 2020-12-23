d30041675c744c3b06bb0bd1e911140b215758f8
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2020-12-23 17:17:22 +0100

    Use express with handlebars and less middleware

---
+++ server.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) BSI Business Systems Integration AG. All rights reserved.
+ * http://www.bsiag.com/
+ */
+const express = require('express'); // Creates our express server
+const handlebars = require('express-handlebars');
+const less = require('express-less');
+const app = express();
+const port = 8080; // Serves static files (we need it to import a css file)
+
+app.locals = {
+  scout: {
+    version: 11
+  },
+  urls: {
+    eclipse: {
+      home: 'http://www.eclipse.org/',
+      privacyPolicy: 'http://www.eclipse.org/legal/privacy.php',
+      termsOfUse: 'http://www.eclipse.org/legal/termsofuse.php',
+      copyrightAgent: 'http://www.eclipse.org/legal/copyright.php',
+      legal: 'http://www.eclipse.org/legal'
+    }
+  }
+};
+
+// Set-up handlebars engine
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs'
+}));
+
+app.use('/css', less(__dirname + '/css', {debug: true}));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', (req, res) => {
+  // Serves the body of the page aka "main.hbs" to the container aka "index.hbs"
+  res.render('main', {layout: 'index'});
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
