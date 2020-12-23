638b8d1500ac25eb00159414f3525a95734d3ab4
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2020-12-23 16:41:28 +0100

    Added navigation popup (lv2)

--- js/main.js
+++ js/main.js
@@ -42,8 +42,51 @@ function installHandlers() {
   $('#get-started-button').on('click', onGetStartedButtonClick);
 
   $('.demo-app-button').on('click', onDemoAppButtonClick);
+
+  $('.navigation-item.lv1 > .text').on('click', onNavigationItemLv1Click);
+}
+
+function onNavigationItemLv1Click(event) {
+  let $navItem = $(event.currentTarget);
+  let $popup = $navItem.parent().find('.navigation-popup');
+
+  if (!$popup.length) {
+    hideAllNavigationPopups();
+    return;
+  }
+
+  if (!$popup.hasClass('hidden')) {
+    hideNavigationPopup($popup);
+    return;
+  }
+  let clickHandler = onNavigationPopupClickOutside.bind($popup);
+  $popup.removeClass('hidden');
+  $popup.data('clickHandler', clickHandler);
+  setTimeout(() => {
+    $(document).on('click', clickHandler);
+  });
+}
+
+function onNavigationPopupClickOutside(event) {
+  let $popup = this;
+  if ($popup[0] !== event.target) {
+    hideNavigationPopup($popup);
+  }
+}
+
+function hideNavigationPopup($popup) {
+  $popup.addClass('hidden');
+  $(document).off('click', $popup.data('clickHandler'));
+  $popup.removeData('clickHandler');
+}
+
+function hideAllNavigationPopups() {
+  $('.navigation-popup:not(.hidden)').each((index, element) => {
+    hideNavigationPopup($(element));
+  });
 }
 
 $(document).ready(() => {
   installHandlers();
+  hideAllNavigationPopups();
 });
