8bd91611fe75b3f72407945eff1cb9a56fb4961a
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2020-12-23 11:35:12 +0100

    Fixed fade-in/fade-out animation in demo-apps panel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const hideAnimationListener = event => {
+  $(event.target)
+    .removeClass('animate-hide')
+    .addClass('hidden')[0]
+    .removeEventListener('animationend', hideAnimationListener);
+};
+
+const showAnimationListener = event => {
+  $(event.target)
+    .removeClass('animate-show')[0]
+    .removeEventListener('animationend', showAnimationListener);
+};
+
 function onDemoAppButtonClick() {
   let $activeButton = $('.demo-app-button.active');
   $activeButton.removeClass('active');
@@ -5,11 +18,18 @@ function onDemoAppButtonClick() {
   let $button = $(this);
   $button.addClass('active');
 
-  let $visibleContainer = $('.demo-app-container:not(.hidden)');
-  $visibleContainer.addClass('hidden');
-
   let showContainerId = $button.data('showContainer');
-  $('#' + showContainerId).removeClass('hidden');
+  let $divToHide = $('.demo-app-container:not(.hidden)');
+  let $divToShow = $('#' + showContainerId);
+
+  $divToHide
+    .addClass('animate-hide')[0]
+    .addEventListener('animationend', hideAnimationListener);
+
+  $divToShow
+    .removeClass('hidden')
+    .addClass('animate-show')[0]
+    .addEventListener('animationend', showAnimationListener);
 }
 
 function onGetStartedButtonClick() {
