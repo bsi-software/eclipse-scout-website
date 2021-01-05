c8ec99785941bf7194a1b73246afc7fdadbfb692
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2021-01-05 09:55:44 +0100

    Added tool-tips. Fixed many issues from review

--- README.md
+++ README.md
@@ -4,12 +4,29 @@
 * Start http://localhost:8080/
 
 ## TODOs
-* Review mit Claudio
-* Hover-Effekt für Features Grid
-* Icons unten im Footer weg? Gibt's ja schon in der Haupt-Navi, Blog ist fraglich
-* Icon "laptop-launch" oder "space-rocket-flying" bei getting started?
-* Texte verbessern, Finale Texte bei Marketing bestellen
-* Highlight-Color definieren (Farbklecks fehlt)
-* Kleine (farbige) Animation für Raketentriebwerk
-* Animationen definieren, braucht es mehr?
+* SVG Version vom Scout Logo erstellen (PNG ist unscharf)
+  Variante mit Scout-Blau machen
+* Hover-Effekt für Features Grid --> probieren: Titel und Icon einfärben
+  oder 3D mässig hervorheben
+* Texte verbessern, Finale Texte bei Marketing bestellen. Punkt oder nicht
+  bei Kacheln und anderen Texten. Eher mit.
+* Kleine (farbige) Animation für Raketentriebwerk --> ausprobieren
+* DAX Ot als Webfont einbetten
+* v11 auf https://eclipsescout.github.io/11.0/ verlinken
+* Nowrap bei Get Started Titel
+* Prüfen, ob das Copyright unten im Footer korrekt ist (Eclipse statt BSI?)
+* Internet Explorer ggf. Error abfangen / Edge prüfen
+* Schauen ob wir github.io in die Homepage integrieren können.
 
+### Erledigt
+* Manchmal gibt es einen JS-Error beim $divToHide #onDemoAppButtonClick
+* Mehr Abstand zw. Titel Demo Apps und Navigations-Element
+* Tooltips für Scout JS / Classic bei Demo Apps und Getting started
+* Responsive UI an dritter Stelle
+* Icon "laptop-launch" oder "space-rocket-flying" bei getting started? --> Letzteres
+* Weitere Animationen definieren, braucht es mehr? --> Nein
+* Icons unten im Footer weg? Gibt's ja schon in der Haupt-Navi, Blog ist fraglich
+  --> So lassen, aber Streamline Varianten verwenden
+* Demo-Apps: schnellerer Fade bei Überblendung
+* Demo-Apps: Screenshots vertauscht
+* Highlight-Color definieren (Farbklecks fehlt) --> Orange OK
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const TEXTS = {
+  'scout.js': 'Scout JS applications are written in JavaScript. The business logic runs in the browser. An application server' +
+    ' is optional.',
+  'scout.classic': 'Scout Classic applications are written in Java. They implement the client/server principle. The business logic' +
+    ' runs on a Java application server.'
+};
+
 const hideAnimationListener = event => {
   $(event.target)
     .removeClass('animate-hide')
@@ -12,15 +19,23 @@ const showAnimationListener = event => {
 };
 
 function onDemoAppButtonClick() {
+  let $button = $(this);
+  if ($button.hasClass('active')) {
+    return;
+  }
+
   let $activeButton = $('.demo-app-button.active');
   $activeButton.removeClass('active');
-
-  let $button = $(this);
   $button.addClass('active');
 
   let showContainerId = $button.data('showContainer');
   let $divToHide = $('.demo-app-container:not(.hidden)');
   let $divToShow = $('#' + showContainerId);
+
+  // Animation already running --> skip
+  if (!$divToHide.length) {
+    return;
+  }
 
   $divToHide
     .addClass('animate-hide')[0]
@@ -38,12 +53,31 @@ function onGetStartedButtonClick() {
   });
 }
 
+/**
+ * Create the additional tooltip DIV only once. The rest is done with CSS only.
+ */
+function onTooltipMouseover(event) {
+  let $tooltip = $(event.currentTarget);
+  let tooltipTextKey = $tooltip.data('tooltipText');
+
+  if (!$tooltip.find('.tooltip-text').length) {
+    let tooltipText = TEXTS[tooltipTextKey];
+    if (!tooltipText) {
+      tooltipText = 'Undefined textKey "' + tooltipTextKey + '"';
+    }
+    let $tooltipText = $('<span>').addClass('tooltip-text').text(tooltipText);
+    $tooltipText.appendTo($tooltip);
+  }
+}
+
 function installHandlers() {
   $('#get-started-button').on('click', onGetStartedButtonClick);
 
   $('.demo-app-button').on('click', onDemoAppButtonClick);
 
   $('.navigation-item.lv1 > .text').on('click', onNavigationItemLv1Click);
+
+  $('.tooltip').one('mouseover', onTooltipMouseover);
 }
 
 function onNavigationItemLv1Click(event) {
